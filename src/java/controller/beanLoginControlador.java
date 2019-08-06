4df7ca889d64b7bbf57bb7cec126f463c8882eb8
Jose_Cespedes <Jose_Cespedes@W103N706S2.mshome.net>
2019-08-05 22:56:54 -0600

    login

--- src/java/controller/beanLoginControlador.java
+++ src/java/controller/beanLoginControlador.java
@@ -8,6 +8,10 @@ package controller;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import model.Persona;
+import model.PersonaDB;
 
 /**
  *
@@ -16,11 +20,70 @@ import java.io.Serializable;
 @Named(value = "beanLoginControlador")
 @SessionScoped
 public class beanLoginControlador implements Serializable {
-
+    String login;
+    String password;
+    Persona oPersona;
+    Persona usuarioIngresado;
+    
     /**
      * Creates a new instance of beanLoginControlador
      */
     public beanLoginControlador() {
     }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Persona getoPersona() {
+        return oPersona;
+    }
+
+    public void setoPersona(Persona oPersona) {
+        this.oPersona = oPersona;
+    }
+
+
     
+     public void autenticar(){
+         try {
+             oPersona = new PersonaDB().loginPersona(this.login, this.password);
+
+             if (oPersona != null) {
+                 FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("Usuario", usuarioIngresado);
+                 switch (oPersona.getCOD_ROL()) {
+                     case 1:
+                         FacesContext.getCurrentInstance().getExternalContext().redirect("inicioAdministrador.xhtml");
+                         break;
+                     case 2:
+                         FacesContext.getCurrentInstance().getExternalContext().redirect("inicioInstructor.xhtml");
+                         break;
+                     default:
+                         FacesContext.getCurrentInstance().getExternalContext().redirect("inicioDeportista.xhtml");
+                        break;
+                }
+
+            } else {
+                FacesContext context = FacesContext.getCurrentInstance();
+                context.addMessage(null, new FacesMessage("Usuario ó Clave incorrecta!"));
+            }
+
+        } catch (Exception e) {
+            FacesContext context2 = FacesContext.getCurrentInstance();
+            context2.addMessage(null, new FacesMessage("Error", e.toString()));
+        }
+
+    }
 }
